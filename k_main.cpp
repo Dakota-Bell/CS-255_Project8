970ad4df5bf1e42371b5ee65e35e7ac200d962f4
kennedy1743 <152230794+kennedy1743@users.noreply.github.com>
2023-11-28 18:50:39 -0600

    Create k_main.cpp

---
+++ k_main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include "linkedListStack.h"
+#include "queueLinked.h"
+#include "linkedList.h"
+#include "Book.h"
+
+using namespace std;
+
+void printStackNames(linkedListType<string>);
+
+int menu()
+{
+    int choice;
+    cout << "Make a Selection: " << endl;
+    cout << "\t1. Create a stack" << endl;
+    cout << "\t2. Delete a stack" << endl;
+    cout << "\t3. Print Stack Names" << endl;
+    cout << "\t4. Update Active Stack" << endl;
+    cout << "***Select for Active Stack: None Selected***" << endl;
+    cout << "\t5. Load Books From File" << endl;
+    cout << "\t6. Add Book to Stack" << endl;
+    cout << "\t7. Print Stack Top" << endl;
+    cout << "\t8. Print Stack Top" << endl;
+    cout << "\t9. Remove Book " << endl;
+    cout << "\t10. Quit" << endl;
+    cin >> choice;
+
+    return choice;
+}
+
+int main()
+{   
+    int stackSelect;
+    string name;
+    string activeStack = "None Selected";
+    linkedListType<string> stackName;
+    linkedQueueType<linkedStackType <Book*> > stackQueue;
+    linkedStackType<Book*> bookStack;
+    int choice = 0;
+    cout << "Active Stack: None Selected" << endl;
+    while(choice != 10)
+    {
+        cout << "Active Stack: " << activeStack;
+        choice = menu();
+        switch (choice)
+        {
+        case 1:
+            
+            break;
+        case 2:
+            if(_____.isEmpty())
+                cout << "No Intend2Read Stacks Available." << endl;
+            else
+            {
+                cout << "Enter a name for your stack: " << endl;
+                getline(cin, name);
+                _____.insert(name); // string list of names
+                activeStack = name;
+                _____.initializeList(); // separate list of book stacks.
+            }
+            break;
+        case 3:
+            if(_____.isEmpty())
+                cout << "No Intend2Read Stacks Available." << endl;
+            else
+            {
+                ____.print();
+            }
+            break;
+        case 4:
+            cout << "***Select a Stack by Number: ***" << endl;
+            if(_____.isEmpty())
+            {
+                cout << "No Intend2Read Stacks Available" << endl;
+            }
+            else
+            {
+                ____.print();
+                cout << "-1 to CANCEL." << endl;
+                cin >> stackSelect;
+                if(stackSelect == -1)
+                {
+                    cout << "Active Stack NOT Updated" << endl;
+                }
+            }
+            break;
+        case 5:
+            // loadBooks();
+            break;
+        case 6:
+            // addBook();
+            break;
+        case 7:
+            // top();
+        
+        default:
+            break;
+        }
+    }
+    return 0;
+}
