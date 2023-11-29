23424d10fb7af1d29d2edc1202d3bf3825a87283
Dakota Bell <115654996+Dakota-Bell@users.noreply.github.com>
2023-11-28 19:59:30 -0600

    Update linkedListStack.h
    changed copyList so that it does not initialize the list you're trying to copy an empty list with an empty list

--- linkedListStack.h
+++ linkedListStack.h
@@ -2,6 +2,7 @@
 #include "queueLinkedList.h"
 #include <string>
 using namespace std;
+#include <csassert>
 
 template<class Type>
 
@@ -43,109 +44,118 @@ class linkedListType
 		}
 		
 		
-	void insert(const Type& item)
-	{
-		nodeType<Type>* newNode;
-		
-		newNode = new nodeType<Type>;
-		
-		newNode->info = item;
-		newNode->link = first;
-		first = newNode;
-		count++;
-		
-		if(last == nullptr)
+		void insert(const Type& item)
 		{
-			last = newNode;
-		}
-	}
-	
-	Type top() const
-	{
-		return stackTop->info;
-	}
-		
-	void pop()
-	{
-		nodeType<Type> *temp;// to hold value of the top to deallocate
-	
-		if(stackTop != nullptr) // while stackTop has values
-		{
-			temp = stackTop; // sets temp the value at the top of the stack
-			stackTop = stackTop->link; // stackTop is assigned the value that it links to 
+			nodeType<Type>* newNode;
 			
-			delete temp; // delete the old stackTop
+			newNode = new nodeType<Type>;
+			
+			newNode->info = item;
+			newNode->link = first;
+			first = newNode;
+			count++;
+			
+			if(last == nullptr)
+			{
+				last = newNode;
+			}
 		}
-	}
 		
-	copyList(const linkedListType& otherList)
-	{
-	    nodeType<Type> *newNode; //pointer to create a node
-	    nodeType<Type> *current; //pointer to traverse the list
-	
-	    if (stackTop != nullptr) //if the list is nonempty, make it empty
-	       //destroyList();
-	
-	    if (otherList.first == nullptr) //otherList is empty
-	    {
-	        first = nullptr;
-	        last = nullptr;
-	        count = 0;
-	    }
-	    else
-	    {
-	        current = otherList.first; //current points to the
-	                                   //list to be copied
-	        count = otherList.count;
-	
-	            //copy the first node
-	        first = new nodeType<Type>;  //create the node
-	
-	        first->info = current->info; //copy the info
-	        first->link = nullptr;        //set the link field of
-	                                   //the node to nullptr
-	        last = first;              //make last point to the
-	                                   //first node
-	        current = current->link;     //make current point to
-	                                     //the next node
-	
-	           //copy the remaining list
-	        while (current != nullptr)
-	        {
-	            newNode = new nodeType<Type>;  //create a node
-	            newNode->info = current->info; //copy the info
-	            newNode->link = nullptr;       //set the link of
-	                                        //newNode to nullptr
-	            last->link = newNode;  //attach newNode after last
-	            last = newNode;        //make last point to
-	                                   //the actual last node
-	            current = current->link;   //make current point
-	                                       //to the next node
-	        }
-	    }
-	}
-
-		
-	linkedListType() // constructor
-	{
-		stackTop = nullptr;
-	}
-		
-	//template <class Type>
-	const linkedListType<Type>&operator=(linkedQueueType<Type>& otherQueue)
-	{
-    //Write the definition of to overload the assignment operator
-    
-	    if(this != &otherQueue) // if me (pointer) is not equal to the address of otherQueue
-	    {
-	    	copyList(otherQueue); // make a copy of the otherQueue
+		Type top() const
+		{
+			assert(stackTop != nullptr);
+			
+			return stackTop->info;
 		}
-		return *this; // return me (pointer) dereferenced
-
-	} //end assignment operator
+			
+		void pop()
+		{
+			nodeType<Type> *temp;// to hold value of the top to deallocate
 		
-	~linkedListType() // destructor
-	{
-		initializeStack(); // no need to rewrite what has already been written
-	}
+			if(stackTop != nullptr) // while stackTop has values
+			{
+				temp = stackTop; // sets temp the value at the top of the stack
+				stackTop = stackTop->link; // stackTop is assigned the value that it links to 
+				
+				delete temp; // delete the old stackTop
+			}
+			else
+			{
+				cout << "Cannot remove from an empty stack." << endl;
+			}
+		}
+			
+		void copyList(const linkedListType& otherList)
+		{
+		    nodeType<Type> *newNode; //pointer to create a node
+		    nodeType<Type> *current; //pointer to traverse the list
+		
+		    //if (stackTop != nullptr) //if the list is nonempty, make it empty
+				//initializeStack();
+		
+		    if (otherList.first == nullptr) //otherList is empty, initialize the bitch
+		    {
+		        first = nullptr;
+		        last = nullptr;
+		        count = 0;
+		    }
+		    else
+		    {
+		        current = otherList.first; //current points to the
+		                                   //list to be copied
+		        count = otherList.count;
+		
+		            //copy the first node
+		        first = new nodeType<Type>;  //create the node
+		
+		        first->info = current->info; //copy the info
+		        first->link = nullptr;        //set the link field of
+		                                   //the node to nullptr
+		        last = first;              //make last point to the
+		                                   //first node
+		        current = current->link;     //make current point to
+		                                     //the next node
+		
+		           //copy the remaining list
+		        while (current != nullptr)
+		        {
+		            newNode = new nodeType<Type>;  //create a node
+		            newNode->info = current->info; //copy the info
+		            newNode->link = nullptr;       //set the link of
+		                                        //newNode to nullptr
+		            last->link = newNode;  //attach newNode after last
+		            last = newNode;        //make last point to
+		                                   //the actual last node
+		            current = current->link;   //make current point
+		                                       //to the next node
+		        }
+		    }
+		}
+	
+			
+		linkedListType() // constructor
+		{
+			stackTop = nullptr;
+			first = nullptr;
+			last = nullptr;
+		}
+			
+		//template <class Type>
+		linkedListType<Type>& operator=(const linkedListType<Type>& otherQueue)
+		{
+	    //Write the definition of to overload the assignment operator
+	    
+		    if(this != &otherQueue) // if me (pointer) is not equal to the address of otherQueue
+		    {
+		    	copyList(otherQueue); // make a copy of the otherQueue
+			}
+			return *this; // return me (pointer) dereferenced
+	
+		} //end assignment operator
+		
+			
+		~linkedListType() // destructor
+		{
+			initializeStack(); // no need to rewrite what has already been written
+		}
 };
